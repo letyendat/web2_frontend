0f637fc0765c5cad1af425f59ae81ce156a3ea11
datle <letyendat@gmail.com>
2022-11-20 20:04:47 +0700

    react-route-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-/* eslint-disable react/jsx-filename-extension */
-/* eslint-disable react/react-in-jsx-scope */
 import './App.css';
 import Header from './components/Header';
+import { Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      {/* <Route path="/"></Route> */}
     </div>
   );
 }
